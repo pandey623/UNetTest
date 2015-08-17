afca83d34b01600647e0e7ac61019c3aea57eb33
midorimushi <hirotada@gmail.com>
2015-08-18 06:33:04 +0900

    multiplayer game is working now! yay!

---
+++ Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+public class Player_NetworkSetup : NetworkBehaviour {
+    [SerializeField]
+    Camera FPSCharacterCam;
+
+    [SerializeField]
+    AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer) {
+            GameObject.Find("SceneCamera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+
+            GetComponent<FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audioListener.enabled = true;
+        }
+	}
+}
